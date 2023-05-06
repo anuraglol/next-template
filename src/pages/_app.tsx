959d5bc47853ac7ed4029b018babc9f0d5a05986
Anurag <kr.anurag24@gmail.com>
2023-05-06 14:15:02 +0530

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster
+        richColors
+        style={{
+          fontFamily: "var(--font-sans)",
+        }}
+      />
+      <ReactQueryDevtools />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
